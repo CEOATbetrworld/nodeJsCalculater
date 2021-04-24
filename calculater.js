3d4b2a291859a938d31d88900741e6d6ccf9293e
DHRUV KUMAR <dkiphones620@gmail.com>
2021-04-24 19:35:39 +0530

    added body-parser

--- calculater.js
+++ calculater.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.listen(3000,function(){
+app.post("/",function(req,res){
+   console.log(req.body);
+})
+
+app.listen(3000, function () {
     console.log("server started on port 3000");
 })
