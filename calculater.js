de3cb64e526408efd283a6c718f8349ac38bab15
DHRUV KUMAR <dkiphones620@gmail.com>
2021-04-24 19:43:12 +0530

    added add logic

--- calculater.js
+++ calculater.js
@@ -11,8 +11,17 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.post("/",function(req,res){
-   console.log(req.body);
+app.post("/", function (req, res) {
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The sum of two mumber you entered is " + result);
+
+
+
 })
 
 app.listen(3000, function () {
