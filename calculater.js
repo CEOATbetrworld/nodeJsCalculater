d0dbfe7fb442ba1aa577cae768274219ddb5534b
DHRUV KUMAR <dkiphones620@gmail.com>
2021-04-26 10:23:21 +0530

    added bmi calculator route

--- calculater.js
+++ calculater.js
@@ -27,3 +27,20 @@ app.post("/", function (req, res) {
 app.listen(3000, function () {
     console.log("server started on port 3000");
 })
+
+app.get("/bmicalculater", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculater.html")
+})
+
+app.post("/bmicalculater", function (req, res) {
+
+    var height = Number(req.body.height);
+    var weight = Number(req.body.weight);
+
+    var heightInSqMeters = Math.pow((height / 100), 2);
+
+    var bmi = weight / heightInSqMeters;
+
+    res.send("Your BMI is " + bmi);
+
+})
